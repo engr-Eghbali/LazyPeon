28fe2a28d2fb83832c62ce9627e0ca950c7ed426
reza <reza@>
2019-04-05 13:19:20 +0100

    side menu sketch...

--- template/js/script.js
+++ template/js/script.js
@@ -7,7 +7,38 @@ function slideShow(){
     for (i=0;i<childs.length;i++){
         childs[i].style.width=(100/n)+"%";
     }
+   
+}
+//////////////////////////////
 
-    
+//////////////////////////////
+function openSideMenu(){
+
+    var pos=-100;
+    var id=setInterval(function(){
+        
+        if (pos<0){
+         document.getElementById("sideMenu").style.right=pos+"%";
+         pos+=2;
+        }else{
+         clearInterval(id);
+        }
+    },10)
+}
+///////////////////////////////////
+function closeSideMenu(){
+
+    var pos=0;
+    var id=setInterval(function(){
+        
+        if (pos>-100){
+         document.getElementById("sideMenu").style.right=pos+"%";
+         pos-=2;
+        }else{
+         clearInterval(id);
+        }
+    },10)
+
 }
 slideShow();
+
